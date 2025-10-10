f45a840dbf259603d866b95139e341409bd23ade
Danimatar0 <matardani2@gmail.com>
2025-10-10 18:04:30 +0300

    Fix scroll to bottom v1 in product details

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, ScrollRestoration } from "react-router-dom";
 import Index from "./pages/Index";
 import Collection from "./pages/Collection";
 import ProductDetails from "./pages/ProductDetails";
 import NotFound from "./pages/NotFound";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const queryClient = new QueryClient();
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const App = () => {
   // Set basename for GitHub Pages deployment
@@ -20,6 +32,7 @@ const App = () => {
         <Toaster />
         <Sonner />
         <BrowserRouter basename={basename}>
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/collection" element={<Collection />} />
